3fdcf593e9174c0491a42cfe34d9189ac1d5b1d7
German Maximo Montero Sande <german.montero@fing.edu.uy>
2023-08-11 18:32:59 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,20 @@
+enabled = true
+
 //document.addEventListener("DOMContentLoaded", ()=>{
     document.getElementById("div").addEventListener("click", e=>{
-        alert("Hola! Soy el div")
+        if(enabled)
+            alert("Hola! Soy el div");
     });
 //});
+
+function boto() {
+    enabled = false
+    window.setTimeout(() => {
+        enabled=true
+    },1);
+    alert('Hola!');
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    //alert("Termino de cargar")
+});
